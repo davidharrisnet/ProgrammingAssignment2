c6750d9906c61a59c63a86abe0e14f461af66726
Charles Harris <CHarris4@uams.edu>
2017-10-14 22:31:05 -0400

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## The following functions operate on invertible matrices,
+## caching the mean for better performance.
+## 
 
-## Write a short comment describing this function
+
+## makeCacheMatrix
+##  Input: an invertible matrix 
+##  Returns the Vector object containing the matrix,
+##  and helper functions: get(), set(), getmean(), setmean()
 
 makeCacheMatrix <- function(x = matrix()) {
+   aVector <- makeVector(x)  
+   aVector  
 
-}
+  }
 
-
-## Write a short comment describing this function
+## cacheSolve
+## Returns the inverse of the Matrix object returned from 
+##  makeCacheMatrix(), and caches its mean
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inversM <- solve(x$get(), ...)
+	cachemean(x)
+      inversM
 }
+
+
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        } 
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
+
